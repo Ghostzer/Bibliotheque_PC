30f6732d5a1558ddce8ccd4db65855d0ee95f625
ghostzer <djxrico@gmail.com>
2016-12-16 22:59:59 +0100

    work on booking system with user

--- src/mpc/PlatformBundle/Controller/DefaultController.php
+++ src/mpc/PlatformBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace mpc\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use mpc\PlatformBundle\Entity\Reservation;
+use mpc\PlatformBundle\Entity\Utilisateurs;
 
 class DefaultController extends Controller {
 
@@ -14,23 +15,23 @@ class DefaultController extends Controller {
         $bds = $em->getRepository('mpcPlatformBundle:Bd')->findBy([], ['date' => 'DESC']); //trie par date
         $livres = $em->getRepository('mpcPlatformBundle:Livre')->findBy([], ['date' => 'DESC']); //trie par date
         $cds = $em->getRepository('mpcPlatformBundle:Cd')->findBy([], ['date' => 'DESC']); //trie par date
-        
+
 
         return $this->render('mpcPlatformBundle:Default:index.html.twig', array('bds' => $bds, 'livres' => $livres, 'cds' => $cds
         ));
     }
 
-    public function mesReservationsAction(Request $request) {
+    public function ajoutReservationsAction(Request $request) {
 
         $em = $this->getDoctrine()->getManager();
-        
+
         $id_select = $request->get('id');
         $datetoday = new \DateTime();
 
         $objet = $em->getRepository('mpcPlatformBundle:Ouvrage')->find($id_select);
-        
+
         $reservation = new Reservation;
-        
+
         $reservation->setOuvrage($objet);
         $reservation->SetDate($datetoday);
 
@@ -38,20 +39,39 @@ class DefaultController extends Controller {
 
         $reservation->setDate($datetoday);
 
+        //jusqu'ici ça fonctionne sans setter l'user
+        
+//        $currentuser = $this->getUser();
+//        $finduser = $em->getRepository('mpcPlatformBundle:Utilisateurs')->find($currentuser);
+//        $add_user = new Reservation;
+//        $add_user = setUtilisateur($utilisateur);
+
+//        $em->persist($add_user);
+
+
         $em->flush();
-        
-        
+
+
+//        return $this->render('mpcPlatformBundle:Default:ajout_reservations_ok.html.twig', array('currentuser' => $currentuser, 'finduser' => $finduser));
         return $this->render('mpcPlatformBundle:Default:ajout_reservations_ok.html.twig');
     }
-    
-    
+
+    public function mesReservationAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $reservations = $em->getRepository('mpcPlatformBundle:Reservation')->findAll();
+
+        return $this->render('mpcPlatformBundle:Default:mesreservations.html.twig', array('reservations' => $reservations,
+        ));
+    }
+
     public function listeReservationAction() {
         $em = $this->getDoctrine()->getManager();
 
         $reservations = $em->getRepository('mpcPlatformBundle:Reservation')->findAll();
-        
-        return $this->render('mpcPlatformBundle:Default:mesreservations.html.twig', array('reservations' => $reservations,
+
+        return $this->render('mpcPlatformBundle:Default:listereservations.html.twig', array('reservations' => $reservations
         ));
     }
-    
+
 }
